9bdfb0e4a16417f2d6683a391b8ee2a5339950e9
Kiirya Arnold <32293740+arnoldkiirya1@users.noreply.github.com>
2023-05-08 14:53:12 +0300

    Update 7-edges.py

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
-
-# Initialize the variable
 word = "Holberton"
-
-# Get the first three letters
 word_first_3 = word[:3]
-
-# Get the last two letters
 word_last_2 = word[-2:]
-
-# Get the middle word
 middle_word = word[1:-1]
-
-# Print the results using f-strings
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
