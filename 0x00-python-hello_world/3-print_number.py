48ee4475780a4e46109b294141f5ad4473957f02
Kiirya Arnold <32293740+arnoldkiirya1@users.noreply.github.com>
2023-05-08 14:44:40 +0300

    Update 3-print_number.py

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-
-# Set the value of the number
 number = 98
-
-# Print the address with the number using f-string
-print(f"{number} Battery street")
+print(f"{number:d} Battery street")
 
