40e4179c443240608d29a389341db633a33b8bc6
Kiirya Arnold <kiiryarnold@gmail.com>
2023-05-09 01:47:48 -0700

    updates

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(10):
+    for j in range(i+1, 10):
+        if i == 0:
+            print("{:02d}".format(j), end=",")
+        else:
+            print("{:02d}".format(j+i*10), end=",")
+print()
+
