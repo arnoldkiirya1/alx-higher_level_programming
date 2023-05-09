07afbc561d229c75d6c4cf1b171a90ca15e9ab4c
Kiirya Arnold <32293740+arnoldkiirya1@users.noreply.github.com>
2023-05-09 12:34:05 +0300

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,8 @@
 for i in range(10):
     for j in range(i+1, 10):
         if i == 0:
-            print("{:02d}".format(j), end=",")
+            print("{:02d}".format(j), end=", ")
         else:
-            print("{:02d}".format(j+i*10), end=",")
-print()
+            print("{:02d}".format(j+i*10), end=", ")
+print("\b\b ")
 
