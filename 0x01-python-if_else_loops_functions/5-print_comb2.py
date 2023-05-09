d7cb9f9cd61d748701399eb8942aa5a8e7f0f559
Kiirya Arnold <32293740+arnoldkiirya1@users.noreply.github.com>
2023-05-09 12:30:26 +0300

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
+output = ""
 for i in range(100):
-    print("{:02d}".format(i), end="")
+    output += "{:02d}".format(i)
     if i < 99:
-        print(", ", end="")
-    else:
-        print()
-
+        output += ", "
+print(output)
