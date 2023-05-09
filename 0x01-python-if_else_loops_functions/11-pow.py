ef554e668be68eee322c03cf27d17d29206df9fd
Kiirya Arnold <32293740+arnoldkiirya1@users.noreply.github.com>
2023-05-09 12:47:41 +0300

    Update 11-pow.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def pow(a, b):
-    result = 1
-    for i in range(b):
-        result *= a
+    if b >= 0:
+        result = 1
+        for i in range(b):
+            result *= a
+    else:
+        result = 1
+        for i in range(-b):
+            result /= a
     return result
