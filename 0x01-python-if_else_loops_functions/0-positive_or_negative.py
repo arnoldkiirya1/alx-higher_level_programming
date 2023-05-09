33568835e09e1de92392b2c95b9b0c61eecd586d
Kiirya Arnold <32293740+arnoldkiirya1@users.noreply.github.com>
2023-05-09 12:18:12 +0300

    Update 0-positive_or_negative.py

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -6,4 +6,7 @@ if number > 0:
 elif number == 0:
     print(number, "is zero")
 else:
-    print(number, "is negative")
+    last_digit = abs(number) % 10
+    if number < 0:
+        last_digit = -last_digit
+    print(last_digit, "is negative")
