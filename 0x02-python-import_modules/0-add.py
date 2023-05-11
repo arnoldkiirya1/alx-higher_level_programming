829a336f1e3c244d8808cc9dd9f000fba77aa600
Kiirya Arnold <kiiryarnold@gmail.com>
2023-05-11 07:13:15 -0700

    Add

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,10 @@
+# import add function from add_0.py
+from add_0 import add
+
+# assign values to a and b
+a = 1
+b = 2
+
+# call add function with a and b as arguments and print the result
+print('{} + {} = {}'.format(a, b, add(a, b)))
+
