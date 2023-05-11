c5ba23685c2385d501cdff86117df3e0a1132a1e
Kiirya Arnold <32293740+arnoldkiirya1@users.noreply.github.com>
2023-05-11 18:32:51 +0300

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-import sys
-
 if __name__ == "__main__":
-   """Print the addition of all arguments."""
-    total = sum(int(arg) for arg in sys.argv[1:])
+    """Print the addition of all arguments."""
+    import sys
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
     print("{}".format(total))
