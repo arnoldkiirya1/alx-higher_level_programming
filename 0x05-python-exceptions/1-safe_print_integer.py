b3374542a146c63f67c440712499ada8837a74fb
Kiirya Arnold <32293740+arnoldkiirya1@users.noreply.github.com>
2023-05-29 19:47:40 +0300

    Update 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
+    """
+    Safely prints an integer value.
+
+    Args:
+        value (int): Integer value to print.
+
+    Returns:
+        bool: True if value is successfully printed, False otherwise.
+    """
     try:
         print("{:d}".format(value))
         return True
