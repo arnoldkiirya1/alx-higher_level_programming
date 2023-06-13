0de14d9cc15c0dd4205a439b322f8f2ead7f255a
Kiirya Arnold <32293740+arnoldkiirya1@users.noreply.github.com>
2023-06-13 21:18:21 +0300

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,48 +1,42 @@
 #!/usr/bin/python3
+"""
+Module for log parsing scripts.
+"""
+
 
 import sys
 
-def print_stats(file_size, status_codes):
-    print("File size: {}".format(file_size))
-    for status_code in sorted(status_codes.keys()):
-        if status_codes[status_code] > 0:
-            print("{}: {}".format(status_code, status_codes[status_code]))
 
-def parse_line(line):
-    try:
-        line_parts = line.split()
-        file_size = int(line_parts[-1])
-        status_code = int(line_parts[-2])
-        return file_size, status_code
-    except:
-        return None, None
+if __name__ == "__main__":
+    size = [0]
+    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
-def compute_metrics():
-    count = 0
-    file_size = 0
-    status_codes = {
-        200: 0,
-        301: 0,
-        400: 0,
-        401: 0,
-        403: 0,
-        404: 0,
-        405: 0,
-        500: 0
-    }
+    def check_match(line):
+        '''Checks for regexp match in line.'''
+        try:
+            line = line[:-1]
+            words = line.split(" ")
+            size[0] += int(words[-1])
+            code = int(words[-2])
+            if code in codes:
+                codes[code] += 1
+        except ValueError:
+            pass
 
+    def print_stats():
+        '''Prints accumulated statistics.'''
+        print("File size: {}".format(size[0]))
+        for k in sorted(codes.keys()):
+            if codes[k]:
+                print("{}: {}".format(k, codes[k]))
+    i = 1
     try:
         for line in sys.stdin:
-            file_size, status_code = parse_line(line)
-            if file_size and status_code:
-                count += 1
-                file_size += file_size
-                if status_code in status_codes:
-                    status_codes[status_code] += 1
-
-                if count % 10 == 0:
-                    print_stats(file_size, status_codes)
+            check_match(line)
+            if i % 10 == 0:
+                print_stats()
+            i += 1
     except KeyboardInterrupt:
-        print_stats(file_size, status_codes)
-
-compute_metrics()
+        print_stats()
+        raise
+    print_stats()
