1c765c3cfce23271242d58f51d599b98f1ea7b86
Kiirya Arnold <32293740+arnoldkiirya1@users.noreply.github.com>
2023-08-31 15:23:22 +0300

    corrected 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,28 +4,7 @@ This module defines a function that finds a peak in a list of unsorted integers.
 """
 
 def find_peak(list_of_integers):
-    """
-    Finds a peak in a list of unsorted integers.
-    """
-    if not list_of_integers:
-        return None
-    
-    low, high = 0, len(list_of_integers) - 1
-    
-    while low < high:
-        mid = (low + high) // 2
-        if list_of_integers[mid] > list_of_integers[mid + 1]:
-            high = mid
-        else:
-            low = mid + 1
-    
-    return list_of_integers[low]
-
-# Test the function with sample inputs
-if __name__ == "__main__":
-    print(find_peak([1, 2, 4, 6, 3]))
-    print(find_peak([4, 2, 1, 2, 3, 1]))
-    print(find_peak([2, 2, 2]))
-    print(find_peak([]))
-    print(find_peak([-2, -4, 2, 1]))
-    print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
+    """Finds a peak in a list of unsorted integers."""
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
