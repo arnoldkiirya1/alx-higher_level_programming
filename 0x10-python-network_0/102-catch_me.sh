21516f5cd8a7a4e47d0e4f14fd2895761e758789
Kiirya Arnold <32293740+arnoldkiirya1@users.noreply.github.com>
2023-08-31 15:14:06 +0300

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# This script makes a request to 0.0.0.0:5000/catch_me using curl,
-# and displays the body of the response.
-
-# Send a request to the specified URL and display the body of the response
-curl -sLX PUT -d "user_id=98" -H "Origin: HolbertonSchool" "$1"
+# Script that makes a request to causes an specific response
+curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:HolbertonSchool"
