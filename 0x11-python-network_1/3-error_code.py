1384f2bf90556344fcab4befa271e34e4a18e84f
Kiirya Arnold <32293740+arnoldkiirya1@users.noreply.github.com>
2023-09-01 23:28:31 +0300

    corrected 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-import urllib.request
-import urllib.error
+"""
+Python script that takes in a URL, sends a request and displays
+the value of the X-Request-Id variable found in the header
+(handling HTTP errors)
+"""
+from urllib import request, error
 import sys
 
-if __name__ == "__main__":
-    url = sys.argv[1]
 
+if __name__ == "__main__":
     try:
-        with urllib.request.urlopen(url) as response:
-            body = response.read().decode('utf-8')
-        print(body)
-    except urllib.error.HTTPError as e:
-        print(f"Error code: {e.code}")
+        with request.urlopen(sys.argv[1]) as response:
+            body = response.read()
+            print(body.decode('utf-8'))
+    except error.HTTPError as err:
+        print('Error code: {}'.format(err.code))
