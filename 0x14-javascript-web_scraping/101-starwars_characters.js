8fc499b0bf6f59dcbf0ff2f1db3e8de14a5e6709
Kiirya Arnold <32293740+arnoldkiirya1@users.noreply.github.com>
2023-09-27 12:27:20 +0300

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,19 +1,57 @@
 #!/usr/bin/node
+
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    let characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
+
+const movieId = process.argv[2]; // Get the movie ID from the command line argument
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+// Check if a movie ID is provided as an argument
+if (!movieId) {
+  console.error('Usage: ./101-starwars_characters.js <movie_id>');
+  process.exit(1);
+}
+
+// Make a GET request to the SWAPI endpoint to fetch movie data
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+
+  if (response.statusCode === 200) {
+    const movieData = JSON.parse(body);
+    const characters = movieData.characters;
+
+    // Fetch and display character names in the correct order
+    fetchAndDisplayCharacterNames(characters);
+  } else {
+    console.error(`Failed to fetch data. Status code: ${response.statusCode}`);
+    process.exit(1);
   }
 });
 
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
-    }
-  }
+function fetchAndDisplayCharacterNames(characterUrls) {
+  const characterPromises = characterUrls.map(characterUrl => {
+    return new Promise((resolve, reject) => {
+      request.get(characterUrl, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          const characterData = JSON.parse(body);
+          resolve(characterData.name);
+        } else {
+          reject(error || `Failed to fetch character data. Status code: ${response.statusCode}`);
+        }
+      });
+    });
+  });
+
+  Promise.all(characterPromises)
+    .then(characterNames => {
+      characterNames.forEach(name => {
+        console.log(name);
+      });
+    })
+    .catch(error => {
+      console.error(error);
+      process.exit(1);
+    });
+}
